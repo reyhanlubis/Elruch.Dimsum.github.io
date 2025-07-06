b971c00ec1234018014cbc5f560ce6e552af0702
reyhanlubis <muhammadreyhanlubis4@gmail.com>
2025-07-06 19:57:35 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika slack di klik
+document.querySelector("#slack");
+onclick = () => {
+  navbarNav.classList.toggle("active");
+};
